27c18cf00196960f13e8ec3dc70da4943f3a09e6
RDOToole89 <roibinotoole@gmail.com>
2020-10-19 20:56:04 +0200

    add routes to serve json

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const app = express();
+const doctorsJSON = require("./patient-json/doctors.json");
+const patientsJSON = require("./patient-json/patients.json");
+const dbJSON = require("./patient-json/db.json");
+
+console.log(doctorsJSON);
 
 app.get("/", (req, res) => {
   res.send("Hello World!, this is the patients API");
 });
 
 app.get("/patients", (req, res) => {
-  res.send("patients");
+  res.send(patientsJSON);
 });
 
 app.get("/doctors", (req, res) => {
-  res.send("doctors");
+  res.send(doctorsJSON);
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port: 3000");
+app.get("/db", (req, res) => {
+  res.send(dbJSON);
+});
+
+app.listen(4000, () => {
+  console.log("Listening on port: 4000");
 });
