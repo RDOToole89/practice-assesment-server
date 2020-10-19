15b0be04dd33901ac37ab1ebd29fbfcbe8377d28
RDOToole89 <roibinotoole@gmail.com>
2020-10-19 21:10:47 +0200

    add dynamic route for searching single patient by id

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ const doctorsJSON = require("./patient-json/doctors.json");
 const patientsJSON = require("./patient-json/patients.json");
 const dbJSON = require("./patient-json/db.json");
 
-console.log(doctorsJSON);
-
 app.get("/", (req, res) => {
-  res.send("Hello World!, this is the patients API");
+  res.send("<h1>Hello! This is the patients API</h1>");
 });
 
 app.get("/patients", (req, res) => {
   res.send(patientsJSON);
+});
+
+app.get("/patients/:id", (req, res) => {
+  const paramId = req.params.id;
+
+  const patient = patientsJSON.find((patient) => patient.id === paramId);
+  res.send(patient);
 });
 
 app.get("/doctors", (req, res) => {
